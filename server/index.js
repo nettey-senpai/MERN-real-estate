e222da42b754046557aca29ab9f578571d45102d
Jeffrey <netteyjeff@gmail.com>
2024-05-27 16:45:06 +0000

    add sign up api auth + encrypted password using BcryptJS

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { log } from "console";
-import userRouter from "./routes/user.routes.js"
-
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -17,9 +17,21 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.use("/server/user", userRouter)
+app.use("/server/user", userRouter);
+app.use("/server/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
